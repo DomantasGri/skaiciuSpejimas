18099ddf8fa58044e2da3d0842e40399af9cc6d2
Domantas Gricius <domantasgricius03@gmail.com>
2024-07-10 11:06:55 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace numberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1,10);
+            int Spejimas;
+
+            Console.WriteLine("Spekite skaičių iki 10");
+
+            for (int i = 0; i <= 4; i++)
+            {
+                
+                Spejimas = Int32.Parse(Console.ReadLine());
+                if (Spejimas != num)
+                {
+                    Console.WriteLine("Jusu spejimas neteisingas turite dar {0} spėjimus",4-i);
+                    if (Spejimas > num)
+                    {
+                        Console.WriteLine("Jūs spėjote per daug");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Jūs spejote per mažai");
+                    }
+                }
+                else {
+                    Console.WriteLine("Laimėjote");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+    }
+
+}
